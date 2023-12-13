2a81f9f825d1acd58143188fbe2a9403f840a4cb
direc <taccount962@gmail.com>
2023-12-13 17:04:34 +0530

    demo | ci/cd test app

---
+++ src/test/java/com/automation/githubcicdactions/GithubCiCdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automation.githubcicdactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCiCdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
